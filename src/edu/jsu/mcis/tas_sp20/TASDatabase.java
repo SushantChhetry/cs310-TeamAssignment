81a2bd59d1ca3a3922ab88eea56c0afcb27fc5d3
mparajuli <mparajuli@stu.jsu.edu>
2020-02-25 18:41:21 -0600

    Classes and objects created

--- src/edu/jsu/mcis/tas_sp20/TASDatabase.java
+++ src/edu/jsu/mcis/tas_sp20/TASDatabase.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.jsu.mcis.tas_sp20;
 
-/**
- *
- * @author dell
- */
+
 public class TASDatabase {
     
+    // creating objects 
+    
+    public Badge getBadge(String badgeid) {
+        return null;
+    }
+    
+    public Punch getPunch(int punchid){
+        return null;
+    }
+    
+    public Shift getShift(Badge badge){
+        return null;
+    }
+    
+    public Shift getShift (int shiftid){
+        return null;
+    }
+    
 }
+
