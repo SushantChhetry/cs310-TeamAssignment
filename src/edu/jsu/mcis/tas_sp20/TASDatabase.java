aec9e6c6b36cf9085a6b8f04eecb23a70840fdf4
Aaron <alaird@stu.jsu.edu>
2020-02-27 15:08:17 -0600

    Added close methods

--- src/edu/jsu/mcis/tas_sp20/TASDatabase.java
+++ src/edu/jsu/mcis/tas_sp20/TASDatabase.java
@@ -13,31 +13,51 @@ public class TASDatabase {
         
         try{
 
-        /* Identify the Server */
+            /* Identify the Server */
         
-        String server = ("jdbc:mysql://localhost/tas");
-        String username = "WarRoomC";
-        String password = "CS488";
-        System.out.println("Connecting to " + server + "...");
+            String server = ("jdbc:mysql://localhost/tas");
+            String username = "WarRoomC";
+            String password = "CS488";
+            System.out.println("Connecting to " + server + "...");
 
-        /* Load the MySQL JDBC Driver */
+            /* Load the MySQL JDBC Driver */
         
-        Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
         
-        /* Open Connection */
+            /* Open Connection */
 
-        conn = DriverManager.getConnection(server, username, password);
+            conn = DriverManager.getConnection(server, username, password);
         
-        /* Test Connection */
+            /* Test Connection */
 
-        if(conn.isValid(0))
+            if(conn.isValid(0)){
             System.out.println("Connection Successful");
+            }
+        
+            /* Close Database Connection */
+            
+            conn.close();
+        
         }
-
+        
         catch (Exception e) {
             System.err.println(e.toString());
         }
-   }
+        
+        /* Close Other Database Objects */
+        
+        finally {
+            
+            if (resultset != null) { try { resultset.close(); resultset = null; } catch (Exception e) {} }
+            
+            if (pstSelect != null) { try { pstSelect.close(); pstSelect = null; } catch (Exception e) {} }
+            
+            if (pstUpdate != null) { try { pstUpdate.close(); pstUpdate = null; } catch (Exception e) {} }
+            
+        }
+    }
+    
+    
     
 
     
