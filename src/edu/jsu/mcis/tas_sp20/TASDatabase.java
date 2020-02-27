101e8be6bd704a4959bea5b65a69a0d9041577af
Aaron <alaird@stu.jsu.edu>
2020-02-27 11:46:11 -0600

    Connected to Database in TASDatabase.java

--- src/edu/jsu/mcis/tas_sp20/TASDatabase.java
+++ src/edu/jsu/mcis/tas_sp20/TASDatabase.java
@@ -1,8 +1,45 @@
-
 package edu.jsu.mcis.tas_sp20;
 
+import java.sql.*;
 
 public class TASDatabase {
+    
+    Connection conn = null;
+    PreparedStatement pstSelect = null, pstUpdate = null;
+    ResultSet resultset = null;
+    ResultSetMetaData metadata = null;
+    
+    public TASDatabase(){
+        
+        try{
+
+        /* Identify the Server */
+        
+        String server = ("jdbc:mysql://localhost/tas");
+        String username = "WarRoomC";
+        String password = "CS488";
+        System.out.println("Connecting to " + server + "...");
+
+        /* Load the MySQL JDBC Driver */
+        
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        
+        /* Open Connection */
+
+        conn = DriverManager.getConnection(server, username, password);
+        
+        /* Test Connection */
+
+        if(conn.isValid(0))
+            System.out.println("Connection Successful");
+        }
+
+        catch (Exception e) {
+            System.err.println(e.toString());
+        }
+   }
+    
+
     
     // creating objects 
     
